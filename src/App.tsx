e119110bb53043f3e2f7b9b69f0d970ed38038d2
AWeirdDev <aweirdscratcher@gmail.com>
2026-01-28 11:52:12 +0800

    new template

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { state } from "@postact/core";
+import { dependent, state } from "@postact/core";
 
 export function Counter({ initial }: { initial: number }) {
-  const count = state<number>(initial);
-  console.log(count);
+  const $count = state<number>(initial);
 
   function callback() {
-    count.update((v) => v + 1);
+    $count.update((v) => v + 1);
   }
 
-  return <button onclick={callback}>{count}</button>;
+  return (
+    <button
+      onclick={callback}
+      style={{
+        transition: "all 100ms ease",
+        scale: dependent($count, (count) => (count > 0 ? "3" : "1")),
+        fontWeight: dependent($count, (count) =>
+          count > 0 ? "bold" : "normal",
+        ),
+      }}
+    >
+      {$count}
+    </button>
+  );
 }
